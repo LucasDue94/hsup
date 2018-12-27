de9ed9e6f45e0a4c2d32250cd0998f9f3e5b3c74
Franklin Farias <franklin.farias@hospitaldocoracao-al.com.br>
2018-12-27 16:13:04 -0300

    Refatoração da tabela produto - order by Descricao e limite de busca

--- server/grails-app/controllers/br/com/hospitaldocoracaoal/hsup/integracao/ProdutoController.groovy
+++ server/grails-app/controllers/br/com/hospitaldocoracaoal/hsup/integracao/ProdutoController.groovy
@@ -10,10 +10,12 @@ class ProdutoController {
     static responseFormats = ['json', 'xml']
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
-    def index(String termo) {
+    def index(Integer max, String termo) {
+
+        params.max = Math.min(max ?: 10, 100)
 
         def criteria = Produto.createCriteria()
-        List<Produto> produtoList = (List<Produto>) criteria.list() {
+        List<Produto> produtoList = (List<Produto>) criteria.list(params) {
             if (termo != null && !termo.isEmpty()) {
                 or {
                     ilike('id', "%${termo}%")
