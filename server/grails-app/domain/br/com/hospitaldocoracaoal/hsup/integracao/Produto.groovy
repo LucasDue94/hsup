ee0775b51296a2abfbb881721ae7e0fbbbbc4a8c
Franklin Farias <franklin.farias@hospitaldocoracao-al.com.br>
2019-01-07 11:20:52 -0300

    Merge branch 'lucas.due' of http://hcor-dev.hcor.local/git/hsup into franklin.farias
    Criação da unidade de estoque no produto WPD

--- server/grails-app/domain/br/com/hospitaldocoracaoal/hsup/integracao/Produto.groovy
+++ server/grails-app/domain/br/com/hospitaldocoracaoal/hsup/integracao/Produto.groovy
@@ -4,13 +4,19 @@ class Produto implements Serializable {
     String codigo
     String setor
     String descricao
-    String estoque
-    String bloqueado
+    Long estoque
+    String unidade_estoque
 
     static mapping = {
         id generator: 'assigned', composite: ['codigo', 'setor']
         version false
     }
 
-
+    static Long countDistinctProducts(){
+        return  (Long) createCriteria().get {
+            projections {
+                countDistinct 'codigo'
+            }
+        }
+    }
 }
