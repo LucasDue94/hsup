dad294393951280b349ff6104c3145538c7b535e
lucas.due <lucasdue!@#>
2019-01-25 17:16:15 -0300

    23/01

--- client/src/app/almoxarife/almoxarife.component.ts
+++ client/src/app/almoxarife/almoxarife.component.ts
@@ -1,16 +1,11 @@
-import { Component, HostListener, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
-import { FormArray, FormBuilder, FormControl } from "@angular/forms";
+import { Component, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import { AlmoxarifeService } from "../core/almoxarife/almoxarife.service";
 import { Produto } from "../core/produto/produto";
-import { HttpResponse } from "@angular/common/http";
-import { Subject } from "rxjs";
-import { Fabricante } from "../core/fabricante/fabricante";
-import { forEach } from "@angular/router/src/utils/collection";
-import { logger } from "codelyzer/util/logger";
 
 @Component({
     selector: 'almoxarife',
@@ -19,64 +14,75 @@ import { logger } from "codelyzer/util/logger";
 })
 export class AlmoxarifeComponent implements OnInit {
 
+    @ViewChild('stock') stock;
+    @ViewChild('codPro') codPro;
     @Input() itemsRequest = [{descricao: 'Memoria 8GB', qnt: '10', unidade: 'und'},
         {descricao: 'Mouse', qnt: '1', unidade: 'KG'},
         {descricao: 'Pilha', qnt: '1', unidade: 'KG'},
         {descricao: 'Coco', qnt: '1', unidade: 'KG'}];
-    @ViewChild('stock') stock;
-    @ViewChild('codPro') codPro;
-    wpdProducts: Produto[];
-    wpdProductsFiltered = new Map();
-    formControls;
     sector = 'Tecnologia da Informação';
     requestNumber = '0001';
     date = '19/12/2018';
     requestUser = 'Beroaldo da Silva Carneiro';
-    currentInput;
+    wpdProducts: Produto[];
+    wpdProductsFiltered = new Map();
+    form: FormGroup;
+    currentInput = null;
     offset = 0;
-    scroll = 0;
     limit = 10;
     almoxarifeList: Produto[] = [];
+    scrollActived: boolean = false;
 
     constructor(private almoxarifeService: AlmoxarifeService, private fb: FormBuilder, private renderer: Renderer2) {
-        this.formControls = this.fb.group({value: 'myBuilder', disable: false});
     }
 
     ngOnInit() {
-        this.createFormControl();
         this.wpdProducts = [];
+        this.buildForm();
     }
 
-    createFormControl() {
-        for (let i = 0; i < this.itemsRequest.length; i++) this.formControls.addControl('searchField' + i, new FormControl());
+    buildForm() {
+        this.form = this.fb.group({
+            products: this.fb.group({}),
+            cods: this.fb.group({}),
+            stocks: this.fb.group({})
+        });
+
+        this.createControlGroup("products", this.itemsRequest.length);
+        this.createControlGroup("cods", this.itemsRequest.length);
+        this.createControlGroup("stocks", this.itemsRequest.length);
     }
 
-    find(event) {
-        console.log(typeof event == "object");
-        this.currentInput = typeof event == "object" ? event : document.getElementById(event);
-        console.log(this.currentInput);
-        const currentControlName = this.currentInput.getAttribute('ng-reflect-name');
-        const currentControl = this.formControls.get(currentControlName);
+    createControlGroup(name, quantity) {
+        let group;
+        for (let i = 0; i < quantity; i++) {
+            group = this.form.get(name);
+            group.addControl(name.slice(0, -1) + i, new FormControl());
+        }
+    }
 
-        console.log(currentControl);
-        if (this.offset >= 10) {
+    find(input, scrollActived) {
+        this.currentInput = input;
+        const currentControl = this.form.get('products.' + this.currentInput.getAttribute('ng-reflect-name'));
+        if (scrollActived) {
             this.almoxarifeService.search(currentControl.value, this.offset, this.limit).subscribe((almoxarifeList: Produto[]) => {
                 this.almoxarifeList = almoxarifeList;
                 if (this.almoxarifeList.length > 0) {
                     this.almoxarifeList.forEach(item => {
                         this.wpdProducts.push(item);
-                    })
+                    });
                 }
             });
+            this.offset += 10;
         } else {
+            this.offset = 0;
             currentControl.valueChanges
                 .debounceTime(1000)
                 .distinctUntilChanged()
                 .switchMap(inputValue => this.almoxarifeService.search(inputValue, this.offset, this.limit))
                 .subscribe((produtos: Produto[]) => {
                     let current = '';
-                    // this.wpdProductsFiltered.clear();
-                    // this.wpdProducts = [];
+                    this.wpdProductsFiltered.clear();
                     for (const a of produtos) {
                         a['codigo'] = a['codigo'].replace(/\s/g, '');
                         if (!this.wpdProductsFiltered.has(a['codigo'])) {
@@ -87,36 +93,35 @@ export class AlmoxarifeComponent implements OnInit {
                         }
                     }
                     this.wpdProductsFiltered.forEach(v => this.wpdProducts.push(v));
-                    console.log(this.wpdProducts);
                 });
-
         }
-        console.log(this.currentInput.value);
-        this.offset += 10;
+
+        console.log(this.wpdProducts);
         console.log(this.offset);
     }
 
-    clearInputs(event, index) {
-        this.offset = 0;
-            this.codPro = document.getElementById('codPro' + index);
-            this.stock = document.getElementById('stock' + index);
-            if (event.value == '') {
-                event.value = '';
-                this.wpdProducts = [];
-                this.stock.value = '';
-                this.codPro.value = '';
-            }
-        }
-
-    select(event, input, item, index) {
-        if (item.unidade_estoque == undefined) item.unidade_estoque = '';
-        if (event.innerHTML.length > 30) event.innerHTML = event.innerHTML.slice(0, 10) + '...';
-        input.value = event.innerHTML;
-        this.stock = document.getElementById('stock' + index);
-        this.stock.value = item.estoque + ' ' + item.unidade_estoque;
-        this.codPro = document.getElementById('codPro' + index);
-        this.codPro.value = item.codigo;
+    clearInputs(index) {
+        this.form.get('stocks.' + 'stock' + index).reset();
+        this.form.get('cods.' + 'cod' + index).reset();
+        this.form.get('products.' + 'product' + index).reset();
         this.wpdProducts = [];
         this.offset = 0;
     }
+
+    select(item) {
+        const index = this.currentInput.getAttribute('ng-reflect-name').slice(-1);
+        let inputPro, inputSto, inputCod;
+        inputPro = this.form.get('products.' + 'product' + index);
+        inputSto = this.form.get('stocks.' + 'stock' + index);
+        inputCod = this.form.get('cods.' + 'cod' + index);
+        inputPro.setValue(item.descricao);
+        inputSto.setValue(item.estoque);
+        inputCod.setValue(item.codigo);
+        this.wpdProducts = [];
+        this.currentInput = null;
+        this.scrollActived = false;
+        this.offset = 0;
+        // this.clearInputs(index);
+    }
+
 }
